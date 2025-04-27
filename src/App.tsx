98a93497a7175dca0c66b66a22ff16e206ef702c
oichan <makoto.uwaya@gmail.com>
2025-04-27 16:13:55 +0900

    ✨ Google Map API の初期実装を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
+import { APIProvider, Map as GoogleMap } from '@vis.gl/react-google-maps';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <main className="min-h-screen p-8">
-      <h1 className="text-3xl font-bold mb-8">React + TypeScript + Vite</h1>
-      <div className="flex gap-4 items-center">
-        <button
-          type="button"
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </main>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
+      <GoogleMap
+        style={{ width: '100vw', height: '100vh' }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
+    </APIProvider>
   );
 }
