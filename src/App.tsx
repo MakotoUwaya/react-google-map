566453ac7cf074191d1b4d940e6fb52afbe91c4a
oichan <makoto.uwaya@gmail.com>
2025-04-27 16:36:35 +0900

    ✨ Update Google Map default center and zoom level

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ export function App() {
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
       <GoogleMap
         style={{ width: '100vw', height: '100vh' }}
-        defaultCenter={{ lat: 22.54992, lng: 0 }}
-        defaultZoom={3}
+        defaultCenter={{ lat: 35.654615441064365, lng: 139.72304033863742 }}
+        defaultZoom={16}
         gestureHandling={'greedy'}
         disableDefaultUI={true}
       />
